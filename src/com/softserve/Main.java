4a4ce8ef7d7e40e38b460a6e29109bca6b3bb58b
Liubomyr Izdepskyi <41821541+Liubomyr17@users.noreply.github.com>
2020-10-18 23:36:58 +0300

    Add files via upload

---
+++ src/com/softserve/Main.java
@@ -0,0 +1,27 @@
+package com.softserve;
+
+public class Main {
+    public static void main(String[] args) {
+        // Solution 1
+        int n = 7;
+
+        System.out.print("The sum is ");
+        for (int sum = 1; sum <= n; sum++) {
+            System.out.print(sum);
+        }
+    }
+}
+// Solution 2
+//        int n = 7;
+//        int sum = 0;
+//        System.out.print("The sum is ");
+//        while (sum < n) {
+//            sum++;
+//        System.out.print(sum);
+//        }
+//    }
+
+
+
+
+
